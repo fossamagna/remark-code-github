5f9afedac038fcf3e773dbb8f6a31edcba76b09b
MURAKAMI Masahiko <fossamagna2@gmail.com>
2025-01-02 23:28:03 +0900

    chore: migrate from Jest to Vitest and update TypeScript configuration (#37)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,9 @@
+import { describe, expect, it } from "vitest";
 import codeGitHub from "./index";
-import remark from "remark";
+import { remark } from "remark";
 
 describe("remark-code-github", () => {
   it("import from raw_url using line numbers", async () => {
-
     const input = `\`\`\`ts raw_url=https://raw.githubusercontent.com/fossamagna/amplify-category-console-notification/c465b778a0defed9c046e349174bc2c3c8f35b0b/src/index.ts#L1-L3
 \`\`\``;
 
@@ -14,15 +14,12 @@ import * as path from 'path';
 \`\`\`
 `;
 
-    const file = await remark()
-      .use(codeGitHub)
-      .process(input);
+    const file = await remark().use(codeGitHub).process(input);
 
     expect(String(file)).toEqual(expected);
   });
 
   it("import from raw_url without line numbers", async () => {
-
     const input = `\`\`\`ts raw_url=https://raw.githubusercontent.com/fossamagna/amplify-category-console-notification/c465b778a0defed9c046e349174bc2c3c8f35b0b/src/index.ts
 \`\`\``;
 
@@ -46,9 +43,7 @@ export async function handleAmplifyEvent(context: $TSContext, args: any) {
 \`\`\`
 `;
 
-    const file = await remark()
-      .use(codeGitHub)
-      .process(input);
+    const file = await remark().use(codeGitHub).process(input);
 
     expect(String(file)).toEqual(expected);
   });
